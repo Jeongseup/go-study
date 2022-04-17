df73ccb24b26ec96945d8895936c4f14df1c52f0
Jeongseup <seup87@gmail.com>
2022-04-17 18:04:11 +0900

    go lang study - function#1

--- golang-for-python-developer/operator.go
+++ golang-for-python-developer/operator.go
@@ -6,6 +6,33 @@ func sample() (int, string) {
 	return 0, "something" // 리턴이 두 개 이상이어도 괜찮다.
 }
 
+// 데이터 타입을 뒤에 써준다.
+func plus(a, b int) int {
+	return a + b
+}
+
+func minus(a, b int) int {
+	return a - b
+}
+
+func multiplied(a, b int) int {
+	return a * b
+}
+
+func divided(a, b int) int {
+	return a / b
+}
+
+func plusViod(a, b int) {
+	// defer fmt.Println("plus viod ... ")
+	defer func() {
+		fmt.Println("plus function was ended")
+		fmt.Println("plus function was ended2")
+	}()
+
+	fmt.Println(a + b)
+}
+
 func main() {
 	a := 8
 	b := 4
@@ -33,4 +60,11 @@ func main() {
 	fmt.Println(s)
 
 	fmt.Println(sample())
+
+	fmt.Println(plus(a, b))
+	fmt.Println(minus(a, b))
+	fmt.Println(multiplied(a, b))
+	fmt.Println(divided(a, b))
+
+	plusViod(a, b)
 }
