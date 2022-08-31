e0a7c922af2738f184a2f32ef7bef5f57cd495af
Jeongseup <seup87@gmail.com>
2022-09-01 00:58:18 +0900

    fatal processing for panic error

--- udemy-golang-unit-tests/calculator/calculator_test.go
+++ udemy-golang-unit-tests/calculator/calculator_test.go
@@ -15,50 +15,34 @@ func TestDiscoountCalulator(t *testing.T) {
 
 	testCases := []testCase{
 		{name: "should apply 20", minimumPurchaseAmount: 100, discout: 20, purchaseAmount: 150, expectedAmount: 130},
-		{name: "should apply 40", minimumPurchaseAmount: 100, discout: 1, purchaseAmount: 200, expectedAmount: 160},
+		{name: "should apply 40", minimumPurchaseAmount: 100, discout: 20, purchaseAmount: 200, expectedAmount: 160},
 		{name: "should apply 60", minimumPurchaseAmount: 100, discout: 20, purchaseAmount: 350, expectedAmount: 290},
 		{name: "should not apply", minimumPurchaseAmount: 100, discout: 20, purchaseAmount: 50, expectedAmount: 50},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			tempCalculator := NewDiscountCalculator(tc.minimumPurchaseAmount, tc.discout)
-			tempAmount := tempCalculator.Calculator(tc.purchaseAmount)
+			calculator, err := NewDiscountCalculator(tc.minimumPurchaseAmount, tc.discout)
+			if err != nil {
+				// Fail+log
+				// t.Errorf("could not instantiate the calculator %s", err.Error())
+				// t.FailNow()
+				// Fatal = Fail + log
+				t.Fatalf("could not instantiate the calculator %s", err.Error())
+			}
 
-			if tempAmount != tc.expectedAmount {
-				t.Errorf("expected %v, got %v", tc.expectedAmount, tempAmount)
+			amount := calculator.Calculator(tc.purchaseAmount)
+
+			if amount != tc.expectedAmount {
+				t.Errorf("expected %v, got %v", tc.expectedAmount, amount)
 			}
 		})
 	}
 }
 
-// func Test_DiscountApplied(t *testing.T) {
-// 	calculator := NewDiscountCalculator(100, 20)
-// 	amount := calculator.Calculator(150)
-
-// 	if amount != 130 {
-// 		t.FailNow()
-// 	}
-// }
-
-// func Test_DiscountMultipliedByTwoApplied(t *testing.T) {
-// 	calculator := NewDiscountCalculator(100, 20)
-// 	amount := calculator.Calculator(200)
-
-// 	if amount != 160 {
-// 		t.Errorf("expected 160, got %v", amount) // Error = Log + Fail
-// 	}
-// }
-
-// func Test_DiscountNotApplied(t *testing.T) {
-// 	calculator := NewDiscountCalculator(100, 20)
-// 	amount := calculator.Calculator(50)
-
-// 	if amount != 50 {
-// 		// t.Logf("expected 50, got %v", amount)
-// 		// t.Fail()
-
-// 		// same as above
-// 		t.Errorf("expected 50, got %v. failed because the discount was not expected", amount) // Error = Log + Fail
-// 	}
-// }
+func TestDiscountCalculatorShoudFailWithZeroMinimumAmount(t *testing.T) {
+	_, err := NewDiscountCalculator(0, 20)
+	if err == nil {
+		t.Fatalf("should not create the calculator with zero value")
+	}
+}
